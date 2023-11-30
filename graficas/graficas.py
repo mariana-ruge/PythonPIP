9c0ee75c4f79dc98108bb724e1ff48315f353bcc
mariana-ruge <mariana.ruge@hotmail.com>
2023-11-30 18:59:21 -0500

    Hemos creado los archivos graficas.py, y main.py

---
+++ graficas/graficas.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+#Función que genera un chart
+def generar_pychart():
+    etiquetas = ['A', 'B', 'C']
+    valores = [200, 100, 300]
+
+    fig, ax = plt.subplots()
+    ax.pie(valores, labels=etiquetas)
+    plt.savefig('pie.png')
+    plt.close()
---
+++ graficas/main.py
@@ -0,0 +1,7 @@
+import graficas
+
+def run():
+    graficas.generar_pychart()
+
+if __name__ == "__main__":
+    run()
